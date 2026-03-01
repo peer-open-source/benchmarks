53ae023fb77f17784ba93eff499edfbfb2e97420
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-03-01 02:55:55 -0800

    Update x-test-a.tcl

--- benchmarks/frame-0028/x-test-a.tcl
+++ benchmarks/frame-0028/x-test-a.tcl
@@ -12,35 +12,36 @@ set J  [expr {$I*2}]
 # Equivalent distributed couple
 set m  [expr {($h/2)*$w}]
 
-model basic -ndm 3 -ndf 6
-node 1 0 0 0 
-node 2 [expr {$L/2}] 0 0 
-node 3 $L 0 0 
-fix 1 1 1 1 1 1 1 
+  model basic -ndm 3 -ndf 6
+  node 1 0 0 0 
+  node 2 [expr {$L/2}] 0 0 
+  node 3 $L 0 0 
+  fix 1 1 1 1 1 1 1 
 
-geomTransf Linear 1 0 -1 0 
-section ElasticFrame 1 \
-      -E $E -G $G \
-      -A $A -Ay $Ay -Az $Az \
-      -J $J -Iy $I  -Iz $I
+  geomTransf Linear 1 0 -1 0 
+  section ElasticFrame 1 \
+        -E $E -G $G \
+        -A $A -Ay $Ay -Az $Az \
+        -J $J -Iy $I  -Iz $I
 
-element ExactFrame 1 {1 2 3} -section 1 -transform 1
+  element ExactFrame 1 {1 2 3} -section 1 -transform 1
 
-pattern Plain 1 Constant 
+  pattern Plain 1 Constant 
 
-eleLoad Frame Heaviside \
-  -basis local \
-  -force "$w 0 0" \
-  -couple "0 -$m 0" \
-  -pattern 1 \
-  -elements 1
+  eleLoad Frame Heaviside \
+    -basis local \
+    -force "$w 0 0" \
+    -couple "0 -$m 0" \
+    -pattern 1 \
+    -elements 1
 
-# Analyze
-analysis Static
-analyze 1
-reactions
+  # Analyze
+  analysis Static
+  analyze 1
+  reactions
 
 
-verify value [nodeReaction 1 6] [expr {$m*$L}] 1e-4
-verify value [nodeDisp 3 6] [expr {-($m*$L*$L/(2*$E*$I))}] 1e-4
-verify value [nodeDisp 3 2] [expr {-($m*$L*$L*$L/(3*$E*$I))}] 1e-4
+  verify value [nodeDisp 3 6] [expr {-($m*$L*$L/(2*$E*$I))}] 1e-4 "Displacement"
+  verify value [nodeDisp 3 2] [expr {-($m*$L*$L*$L/(3*$E*$I))}] 1e-4 "Displacement"
+  verify value [nodeReaction 1 6] [expr {$m*$L}] 1e-4
+  wipe
