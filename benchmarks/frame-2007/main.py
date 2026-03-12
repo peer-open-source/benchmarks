c2f7a46936086956d7707f7c8ef763e44634bab2
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-03-12 10:55:12 -0700

    Update main.py

--- benchmarks/frame-2007/main.py
+++ benchmarks/frame-2007/main.py
@@ -1,191 +1,153 @@
 #
 # Sep 27, 2025
 #
+# Hjelmstad's shear link #4
+#
+
 import os
+import time
 import veux
-from veux.motion import Motion
+from xara import Material
 from xara.benchmarks import Prism
 import xara.units.iks as units
 from xara.helpers import find_node
-from xsection.library import from_aisc
-import numpy as np
+from xsection.library import from_aisc, WideFlange
 import matplotlib.pyplot as plt
+# plt.style.use("typewriter")
 
-verbose = True 
-if verbose:
-    tqdm = lambda x: x
-else:
-    from tqdm import tqdm
+verbose = False
 
-def section(model, tag, shape, material):
-    model.section("ShearFiber", tag, GJ=0)
-    m1 = {k: v for k, v in material[0].items() if k != "type"}
-    m2 = {k: v for k, v in material[1].items() if k != "type"}
-    model.material(material[0]["type"], 1, **m1)
-    model.material(material[1]["type"], 2, **m2)
+from model import create_section, analyze
+from post import PlotResponse
 
-    for fiber in shape.create_fibers(warp=True, shear=True):
-        if abs(fiber["z"]) > (shape.d/2-shape.tf):
-            # Flange
-            model.fiber(**fiber, material=1, section=tag)
-        else:
-            # Web
-            model.fiber(**fiber, material=2, section=tag)
-
-
-def analyze(model, nn, motion=None):
-    # Loading
-    step = 80
-
-    model.pattern("Plain", 1, "Linear", load={nn: (0,0,1,0,0,0)})
-    model.system("BandGeneral")
-    # model.algorithm("NewtonLineSearch", 0.8)
-    model.analysis("Static")
-    model.test("Energy", 1e-17, 40, 2)
-
-    history = [
-        0.55, -0.5, 
-        1.08, -1, 1, -1,
-        1.6, -1.6, 1.6, -1.6,
-        2.15, -2.1, 2.1, -2.1,
-        2.6, -2.6, 2.6, -2.6,
-        3.2, -3.2, 3.22
-    ]
-    u = [0]
-    V = [0]
-    sign = -1
-    try:
-        for point in history:
-            sign *= -1
-
-            model.integrator("DisplacementControl", nn, 3, 
-                            point/step/5)
-                            #  *(3, point/step/40, 5*point/step))
-            print(point)
-
-            while model.state.u(nn,3) * sign < abs(point):
-                if model.analyze(1) != 0:
-                    print(f"Failed at time = {model.state.time} with {point = }")
-                    return u, V
-                if motion:
-                    motion.draw_sections(position=model.state.u, rotation=model.nodeRotation)
-                    motion.advance(len(u)/step)
-                u.append(model.state.u(nn,3))
-                V.append(model.state.time)
-    except KeyboardInterrupt:
-        pass
-
-    return u, V
 
 
 if __name__ == "__main__":
-    size = 40
-    shape = from_aisc("W18x40", units=units, mesh_scale=1/size)
-    print(shape.summary())
+    from xara.post import FiberStress, NodalAverage
+    link_name = "Hjelmstad"
 
-    flange = {
-        "E":  28.0e3*units.ksi,
-        "Fy": 35.0*units.ksi,
-        "Fu": 58.5*units.ksi,
-    }
-    web = {
-        "E":  28.3e3*units.ksi,
-        "Fy": 39.5*units.ksi,
-        "Fu": 60.1*units.ksi,
-    }
-    nu = 0.3
-    # G  = E/(2*(1+nu))
-    material = "GeneralizedJ2" # "J2BeamFiber" # "J2" # "J2Simplified" #
+    Fy = 35.0*units.ksi
+    E = 30e3*units.ksi
+    G = 12e3*units.ksi
 
-    L = 28*units.inch
-    element = os.environ.get("Element", "ForceFrame")
-    transform = os.environ.get("Transform", "Corotational02")
+    material = Material(
+        type="NonlinearJ2", # "J2BeamFiber" #   "J2" # "GeneralizedJ2" # "J2Simplified" #
+        E =  E,
+        G =  G,
+        Fy = Fy,
+        Hiso = 0.002*E,
+        Hkin = 0.002*E
+    )
 
-    #
-    fig, ax = plt.subplots()
-    ax.axhline(0, color='k', lw=0.5)
-    ax.axvline(0, color='k', lw=0.5)
-    ax.set_xlabel("Displacement (in)")
-    ax.set_ylabel("Load (kips)")
-    ax.grid(True)
-    ax.plot(*np.loadtxt("Hjelmstad4.csv", delimiter=",").T, 'k--', label="Experiment")
+    print(f"poisson = {E/(2*G)-1.0}")
+
+    size = 1
+    if link_name == "Hjelmstad":
+        shape = from_aisc("W18x40",
+                        units=units,
+                        mesh_scale=1/size,
+                        material=material,
+                        fillet=True,
+                        mesh_type="T3",
+                        mesher="gmsh")
+        L = 28*units.inch
+    else:
+        shape = WideFlange(
+            tw=1.10*units.inch,
+            d = 33.86*units.inch,
+            b = 23.62*units.inch,
+            tf=1.77*units.inch,
+            material=material,
+        )
+        L = 66*units.inch
+
+    print(shape.summary(shear=False))
 
 
-    for shear in [1]:
+    transform = os.environ.get("Transform", "Linear")
+
+    ##
+    plot_1 = PlotResponse()
+
+    Cases = [
+        dict(element="ForceFrame", shear=1, trace="MS"),
+        dict(element="ForceFrame", shear=0, trace=None),
+        dict(element="ExactFrame", shear=1, trace="energetic"),
+        # dict(element="ExactFrame", shear=1, trace="energetic", order=3),
+        dict(element="ForceFrame", shear=1, trace="energetic"),
+        # dict(element="ForceFrame", shear=1, trace="geometric"),
+        dict(element="ForceFrame", shear=1, trace=None),
+    ]
+
+    ##
+    i = 1
+    for case in Cases:
+        element = case["element"]
+        shear   = case["shear"]
+        trace   = case["trace"]
+        order   = case.get("order", 1)
+
+        # Create section
+        # section = Section(
+        #     type="ShearFiber" if shear else "AxialFiber",
+        #     shape=shape,
+        #     warp=trace,
+        #     material=material,
+        # )
+
+        print(f"Running {element} shear={shear} trace={trace}")
         prism = Prism(shape=shape,
-                      length=L,
-                      boundary=((1,1,1,  1,1,1),
-                                (0,1,0,  1,1,1)),
-                    #   material={
-                    #       "name": "GeneralizedJ2", # "J2BeamFiber", # "J2Simplified", #
-                    #       "E": E,
-                    #       "G": G,
-                    #       "Fy": Fy,
-                    #       "Fs": Fu,
-                    #       "Hiso": 0.00020*E,
-                    #       "Hkin": 0.004*E,
-                    #       "Hsat": 0.005*Fy/(Fu-Fy)*E
-                    #   },
-                      material=[
-                          # Flange
-                          {  
-                            "type": material, 
-                            "E": flange["E"],
-                            "nu": nu, 
-                            "Fy": 0.85*flange["Fy"],
-                            "Fs": flange["Fu"]-0.15*flange["Fy"],
-                            "Hiso": 0.00020*flange["E"], 
-                            "Hkin": 0.004*flange["E"], 
-                            "Hsat": 0.01*flange["Fy"]/(flange["Fu"]-flange["Fy"])*flange["E"]
-                          },
-                          # Web
-                          {
-                            "type": material, 
-                            "E": web["E"], 
-                            "nu": nu, 
-                            "Fy": 0.85*web["Fy"], 
-                            "Fs": web["Fu"] - 0.15*web["Fy"], 
-                            "Hiso": 0.00020*web["E"], 
-                            "Hkin": 0.004*web["E"], 
-                            "Hsat": 0.01*web["Fy"]/(web["Fu"]-web["Fy"])*web["E"]
-                          }
-                      ],
-                      element=element,
-                      section=section, #os.environ.get("Section", "ShearFiber"),
-                      shear=1,
-                      shear_warp=shear,
-                      transform=os.getenv("Transform", transform),
-                      divisions=2,
-                      order=4 if "Exact" in element else 1
+                    length=L,
+                    boundary=((1,1,1,  1,1,1),
+                              (1,1,0,  0,1,1)),
+                    material=material,
+                    element=element,
+                    section=create_section(shape, trace, shear),
+                    shear=shear,
+                    transform=transform,
+                    integration={
+                        "points": 5, "type": "Legendre"
+                    },
+                    divisions=1, # 1
+                    order=order
                 )
 
-        model = prism.create_model()
-        artist = veux.create_artist(model, vertical=3, model_config={
-            "frame_shape": shape
-        })
-        motion = Motion(artist)
-
-        u,V = analyze(model, find_node(model, x=L), motion=motion)
-
-
-        ax.plot(u,V, label=f"{element} size={size}")
-        fig.legend()
-        fig.savefig(f"img/{element}-{transform}-{material}-size{size}-shear{shear}.png", dpi=300)
-        # motion.add_to(artist.canvas)
-        # veux.serve(artist)
-        plt.show()
-
-
-
-        from post import FiberStress
-    
-        artist = veux.create_artist(shape.model)
-        artist.draw_surfaces(
-            field=FiberStress(model, shape, section=1, stress="sxx", element=1)
+        model = prism.create_model(
+            echo_file=open(f"out/{i}.tcl", "w+")
         )
-        veux.serve(artist)
+        model.print(json=f"out/{i}.json")
+
+        plot_1.reset(model, find_node(model, x=L), 3,
+                    label=f"({i}) {'Shear' if shear else 'Euler'}, {'Warping ('+str(trace)+')' if trace else ''}" + f" {element}")
 
 
-    # a = veux.create_artist(model)
-    # a.draw_sections()
-    # veux.serve(a)
+        analyze(model, find_node(model, x=L),
+                trace=trace,
+                shear=shear,
+                verbose=verbose,
+                plots=[plot_1]#+[PlotPlasticSpread(model, elements=[
+                #         ElementFiberYieldMap(model, shape, Fy=Fy, element=1, remember=False),
+                #         ElementFiberYieldMap(model, shape, Fy=Fy, element=2, remember=False)
+                #     ] if False else []) #shear else [])
+                # ]
+        )
+
+        plot_1.draw()
+        i += 1
+
+    plt.tight_layout()
+
+    plot_1.finish()
+    plot_1.ax.figure.savefig(f"img/{element}-size{size}.png", dpi=600)
+    plt.show()
+
+
+    artist = veux.create_artist(shape.model, ndf=1)
+    artist.draw_surfaces()
+    # artist.draw_outlines(state = lambda _: 1)
+    artist.draw_surfaces(
+        field=FiberStress(model, shape, section=1, stress="svm", element=1),
+        state=FiberStress(model, shape, section=1, stress="svm", element=1),
+        scale=1/Fy
+    )
+    veux.serve(artist)
