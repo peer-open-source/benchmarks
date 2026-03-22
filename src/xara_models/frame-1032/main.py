8dcbccecf69b8b4399814b24c8c97a9d53fa6181
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-03-22 13:31:29 -0700

    Create main.py

---
+++ src/xara_models/frame-1032/main.py
@@ -0,0 +1,40 @@
+#
+# Cantilever with channel section and eccentric loading
+#
+# Gruttmann, Sauer, Wagner (2000), Example 6.2
+#
+# Previously 0013
+#
+import os
+
+# External libraries
+import numpy as np
+import matplotlib.pyplot as plt
+# try:
+#     plt.style.use("veux-web")
+# except:
+#     pass
+
+
+
+
+if __name__ == "__main__":
+    from model import Problem
+    # Good:
+    #   1032-D-D-n-fiber-force-02-wagner0-displacements.png
+    
+    p = Problem(origin  = os.environ.get("Origin", "O"),
+                warp_base=os.environ.get("Warping", "n") # "f", "r", "n"
+            )
+    
+    # p.render()
+
+    m = p.create_model(
+                ne = 30,
+                element = os.environ.get("Element", "ExactFrame"),
+                section = os.environ.get("Section", "ShearFiber"),
+                transform = os.environ.get("Transform", "Corotational02"),
+                nen=2)
+    p.analyze(model=m)
+
+    plt.show()
