6638a5fd5874342652f06d72b4e829c3a7f89b7e
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-03-25 13:54:03 -0700

    Update main.py

--- src/xara_models/frame-0012/main.py
+++ src/xara_models/frame-0012/main.py
@@ -1,26 +1,28 @@
-from argparse import ArgumentParser
-
+# xm run frame-0012/main  --save
+import veux
+from xsection.analysis import SaintVenantSectionAnalysis
 
 from run_strain import run_strain
 from run_trace import run_trace
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("-e", "--element", default="ForceFrame",    help="Element type")
-    parser.add_argument("-s", "--shape",   type=str,   default="C10",  help="Shape case")
-    parser.add_argument("-n", "--number",  type=int,   default=1,    help="Number of elements")
-    parser.add_argument("-v", "--poisson", type=float, default=0.3, help="Poisson ratio")
-    parser.add_argument("-r", "--rotate",  default=True,  help="Rotate", action="store_true")
-    parser.add_argument("-t", "--trace",   default="energetic", 
-                        choices=["energetic", "geometric"],
-                        help="Trace type")
-    parser.add_argument("-c", "--center", default="shear", 
-                        choices=["shear", "centroid"], 
-                        help="Coordinate origin")
-    parser.add_argument("-l", "--length", type=float, default=1.5, help="Aspect ratio L/d")
-    options = parser.parse_args()
+    from options import parse_options
+    options = parse_options()
+    Figures = {}
 
-    run_strain(options)
+    shape = options.shape
+    sv = SaintVenantSectionAnalysis(shape)
+    print(sv.summary())
+
+
+    Figures["shape"] = veux.draw_shape(shape, origin=True)
+
+    run_strain(options, Figures=Figures)
 
     run_trace(options)
+
+
+    if options.save:
+        for name, fig in Figures.items():
+            fig.save(f"img/{options.shape_name}_{name}.pdf", dpi=600)#, backend="pgf")
