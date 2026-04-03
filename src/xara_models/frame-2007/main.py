4153c9a9be18af7dfca2a00ae37ea112fb0967a9
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-04-02 22:45:16 -0700

    Update main.py

--- src/xara_models/frame-2007/main.py
+++ src/xara_models/frame-2007/main.py
@@ -6,6 +6,9 @@
 
 import os
 import sys
+from argparse import ArgumentParser
+from pathlib import Path
+import numpy as np
 import veux
 from xara import Material
 from xara.post import PlotConvergenceRate
@@ -22,15 +25,27 @@ verbose = False
 
 from model import create_section, analyze
 from post import PlotResponse
+from xara.post import FiberStress, NodalAverage, PlotConvergenceRate
 
-
+parser = ArgumentParser()
+parser.add_argument("--shape", type=str, default="W03", help="Shape name (W03, H09, RX, H05, etc.)")
+parser.add_argument("--save", action="store_true", default=False, help="Whether to save data and figures")
 
 if __name__ == "__main__":
-    from xara.post import FiberStress, NodalAverage
-    shape_name = "W03" #"Tube" #"Rectangle" #"W03"
+    args = parser.parse_args()
+    Save = args.save
+    shape_name = args.shape #"Tube" #"Rectangle" #"W03"
 
-    if len(sys.argv) > 1:
-        shape_name = sys.argv[1]
+    Cases = [
+        dict(element="ForceFrame", shear=0, trace=None,        tag=1, label=r"Reference~\ref{ref:AxialFiber}"),
+        dict(element="ForceFrame", shear=1, trace=None,        tag=2, label=r"Compatible \eqref{eq:linear-gl-strain-pi}"),
+        dict(element="ForceFrame", shear=1, trace="MS",        tag=3, label=r"Reference~\ref{ref:NDFiber}"),
+        dict(element="ForceFrame", shear=1, trace="energetic", tag=4, label=r"Enhanced \ref{sec:trace-energy}"),
+        dict(element="ForceFrame", shear=1, trace="geometric", tag=5, label=r"Enhanced \ref{sec:trace-cowper}"),
+        # dict(element="ExactFrame", shear=1, trace="energetic", tag=6),
+        # dict(element="ExactFrame", shear=1, trace="energetic", order=3, tag=7),
+    ]
+
 
     Fy = 35.0*units.ksi
     E = 30e3*units.ksi
@@ -92,15 +107,22 @@ if __name__ == "__main__":
     ##
     plot_1 = PlotResponse()
 
-    Cases = [
-        dict(element="ForceFrame", shear=0, trace=None, tag=1),
-        dict(element="ForceFrame", shear=1, trace=None, tag=2),
-        dict(element="ForceFrame", shear=1, trace="MS", tag=3),
-        dict(element="ForceFrame", shear=1, trace="energetic", tag=4),
-        dict(element="ForceFrame", shear=1, trace="geometric", tag=5),
-        dict(element="ExactFrame", shear=1, trace="energetic", tag=6),
-        dict(element="ExactFrame", shear=1, trace="energetic", order=3, tag=7),
-    ]
+
+    ShellColors = iter(["k", "r", "b", "g", "m"])
+    for file in Path("out").glob(f"shell-2007-case?-{shape_name}.txt"):
+        case = file.stem.split("-")[-2][4:]
+        ps, uz, vs, uy = np.loadtxt(file, unpack=True)
+        stride = 200
+        plot_1.ax.plot(uz[::stride], ps[::stride], "o", 
+                label=f"Shells", 
+                color=next(ShellColors),
+                markersize=4,
+                fillstyle="none",
+                # linestyle="-"
+        )
+
+    _,plot_stress = thesis.subplots(1,3, aspect=0.5)
+    plot_stress[0].figure.subplots_adjust(wspace=0.8)
 
     ##
     for case in Cases:
@@ -109,6 +131,7 @@ if __name__ == "__main__":
         shear   = case["shear"]
         trace   = case["trace"]
         order   = case.get("order", 1)
+        case_label = case.get("label", f"Case {i}")
 
 
         print(f"Running {element} shear={shear} trace={trace}")
@@ -134,7 +157,7 @@ if __name__ == "__main__":
         model.print(json=f"out/C{i}_{shape_name}.json")
 
 
-        # plot_cr = PlotConvergenceRate(x_mode="time",ci=95)
+        plot_cr = PlotConvergenceRate(x_mode="time", skip=True)
         # plot_cr.ax.figure.suptitle(f"Case {i}: {element} shear={shear} trace={trace}")
 
         plot_1.reset(model, find_node(model, x=L), 3,
@@ -145,27 +168,37 @@ if __name__ == "__main__":
                 trace=trace,
                 shear=shear,
                 verbose=verbose,
-                plots=[plot_1]#, plot_cr]
+                plots=[plot_1, plot_cr]
         )
 
         plot_1.draw()
         plot_1.save_data(f"out/C{i}_{shape_name}_data.txt")
-        # plot_cr.draw()
-        # plot_cr.finalize()
+        plot_cr.draw()
+        plot_cr.finalize()
 
-        if shear:
+        if False: #shear:
             artist = veux.ShapeArtist(shape, 
-                                      title=f"Case {i}",)
+                                      title=case_label)
             artist.draw_surfaces(
                 field=FiberStress(model, shape, section=1, stress="svm", element=1),
                 cbar_label="Von Mises Stress (ksi)",
             )
             artist.save(f"img/C{i}_{shape_name}_stress.pgf", backend="pgf")
-
+        if i in {2, 4, 5}:
+            artist = veux.ShapeArtist(shape, 
+                                      ax=plot_stress[[2,4,5].index(i)],
+                                      title=case_label)
+            artist.draw_surfaces(
+                field=FiberStress(model, shape, section=1, stress="svm", element=1),
+                cbar_label="Von Mises Stress (ksi)",
+            )
+            # artist.save(f"img/C{i}_{shape_name}_stress.pgf", backend="pgf")
 
     # plt.tight_layout()
 
     plot_1.finish()
+    plot_stress[0].figure.savefig(f"img/{shape_name}_stress.pgf", backend="pgf")
+    # plot_stress[0].figure.tight_layout()
     # plot_1.ax.figure.savefig(f"img/{element}-size{size}.png", dpi=600)
     plt.show()
 
