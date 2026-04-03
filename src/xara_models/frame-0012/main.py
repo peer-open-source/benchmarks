65487f009ec1f78abd55fe60f99dd39bf3ec76df
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-04-02 22:44:12 -0700

    Update main.py

--- src/xara_models/frame-0012/main.py
+++ src/xara_models/frame-0012/main.py
@@ -1,10 +1,13 @@
 # xm run frame-0012/main  --save
 import veux
+import os
 from xsection.analysis import SaintVenantSectionAnalysis
 
 from run_strain import run_strain
 from run_trace import run_trace
+import thesis as plt
 
+os.environ["XARA_FIBER_THREADS"] = "1"
 
 if __name__ == "__main__":
     from options import parse_options
@@ -16,7 +19,10 @@ if __name__ == "__main__":
     print(sv.summary())
 
 
-    Figures["shape"] = veux.draw_shape(shape, origin=True)
+    Figures["shape"] = veux.ShapeArtist(shape)
+    Figures["shape"].draw_surfaces()
+    Figures["shape"].draw_exterior()
+    Figures["shape"].draw_origin(fontsize=16, label="$O$")
 
     run_strain(options, Figures=Figures)
 
@@ -26,3 +32,8 @@ if __name__ == "__main__":
     if options.save:
         for name, fig in Figures.items():
             fig.save(f"img/{options.shape_name}_{name}.pdf", dpi=600)#, backend="pgf")
+
+        with open(f"out/{options.shape_name}.tex", "w") as f:
+            f.write(sv.summary(format="texsection"))
+
+    plt.show()
