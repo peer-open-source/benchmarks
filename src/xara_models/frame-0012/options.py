7ad00a1652e4c9388f930b6569bedf293a030ba8
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-04-02 22:44:18 -0700

    Update options.py

--- src/xara_models/frame-0012/options.py
+++ src/xara_models/frame-0012/options.py
@@ -4,6 +4,27 @@ from xsection._benchmarks import load_shape
 import xara.units.iks as units
 from argparse import ArgumentParser
 
+def ParseFrameSpan():
+    parser = ArgumentParser()
+    parser.add_argument("-e", "--element",    default="ForceFrame",    help="Element type")
+    parser.add_argument("-s", "--shape",      type=str,   default="C10",  help="Shape case", dest="shape_name")
+    parser.add_argument("-n", "--number",     type=int,   default=1,    help="Number of elements")
+    parser.add_argument("-m", "--mesh-scale", type=float, default=None,    help="Mesh scale")
+    parser.add_argument("-v", "--poisson", type=float, default=0.25, help="Poisson ratio")
+    parser.add_argument("-r", "--rotate",  default=None,  help="Rotate", action="store_true")
+    parser.add_argument("-t", "--trace",   default="energetic", 
+                        choices=["energetic", "geometric"],
+                        help="Trace type")
+    parser.add_argument("-o", "--origin", default="shear", 
+                        choices=["shear", "centroid", "default"], 
+                        help="Coordinate origin")
+    parser.add_argument("-l", "--length", type=float, default=1.5, help="Aspect ratio L/d")
+
+    parser.add_argument("--post", nargs="+", default=[], help="Post-processing steps")
+
+    parser.add_argument("--save", default=False, action="store_true", help="Save results")
+
+
 def parse_options():
     parser = ArgumentParser()
     parser.add_argument("-e", "--element",    default="ForceFrame",    help="Element type")
@@ -11,7 +32,7 @@ def parse_options():
     parser.add_argument("-n", "--number",     type=int,   default=1,    help="Number of elements")
     parser.add_argument("-m", "--mesh-scale", type=float, default=None,    help="Mesh scale")
     parser.add_argument("-v", "--poisson", type=float, default=0.25, help="Poisson ratio")
-    parser.add_argument("-r", "--rotate",  default=True,  help="Rotate", action="store_true")
+    parser.add_argument("-r", "--rotate",  default=None,  help="Rotate", action="store_true")
     parser.add_argument("-t", "--trace",   default="energetic", 
                         choices=["energetic", "geometric"],
                         help="Trace type")
@@ -25,10 +46,13 @@ def parse_options():
     parser.add_argument("--save", default=False, action="store_true", help="Save results")
     options = parser.parse_args()
 
+    if options.rotate is None and options.shape_name in ["C10", "W10"]:
+        options.rotate = True
+
 
     print(f"Shape: {options.shape_name}")
 
-    E  = 2.9
+    E  = 2.9 #29e3 # 2.9
     nu = options.poisson
     G = E/(2*(1+nu))
     material = xara.Material(E=E, G=G)
