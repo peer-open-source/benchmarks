49e3ae99c0bddb7f169516c4fa573add11029695
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-09-04 21:37:02 -0700

    add problems from filippou and perez 2024

---
+++ benchmarks/frame-1001/test_frame_1001.py
@@ -0,0 +1,12 @@
+#
+# NAFEMS NL5
+#
+# [1] C. M. Perez and F. C. Filippou, 
+#     “On nonlinear geometric transformations of finite elements,” 
+#     Numerical Meth Engineering, vol. 125, no. 17, Sep. 2024, doi: 10.1002/nme.7506.
+#
+from xara.examples.perez2024nonlinear import Ex_4_2_1
+
+
+def test_Ex_4_2_1():
+    Ex_4_2_1()
---
+++ benchmarks/frame-1006/test_frame_1006.py
@@ -0,0 +1,13 @@
+#
+# Cantilever with moment and force
+#
+# [1] C. M. Perez and F. C. Filippou, 
+#     “On nonlinear geometric transformations of finite elements,” 
+#     Numerical Meth Engineering, vol. 125, no. 17, Sep. 2024, doi: 10.1002/nme.7506.
+#
+from xara.examples.perez2024nonlinear import Ex_4_3_1
+
+
+def test_Ex_4_3_1():
+    Ex_4_3_1()
+
